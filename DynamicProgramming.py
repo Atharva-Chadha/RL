c1ebd28f3108b6bbbf369e1d0cf2de729e87c257
A. Taghavi <taghavizadeh.alireza@gmail.com>
2026-03-04 18:32:52 +0100

    feat: [DynamicProgramming] - Implemented the code
    Code implemented and tested.
    Result:
    Mean reward per timestep under optimal policy: 4.9411764705882355

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -21,26 +21,37 @@ class QValueIterationAgent:
         
     def select_action(self,s):
         ''' Returns the greedy best action in state s ''' 
-        # TO DO: Add own code
-        a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+        a = argmax(self.Q_sa[s])
         return a
         
     def update(self,s,a,p_sas,r_sas):
         ''' Function updates Q(s,a) using p_sas and r_sas '''
-        # TO DO: Add own code
-        pass
+        self.Q_sa[s,a] = np.sum(p_sas * (r_sas + self.gamma * np.max(self.Q_sa, axis=1)))
     
     
 def Q_value_iteration(env, gamma=1.0, threshold=0.001):
     ''' Runs Q-value iteration. Returns a converged QValueIterationAgent object '''
     
     QIagent = QValueIterationAgent(env.n_states, env.n_actions, gamma)
- 
-     # TO DO: IMPLEMENT Q-VALUE ITERATION HERE
-        
-    # Plot current Q-value estimates & print max error
-    # env.render(Q_sa=QIagent.Q_sa,plot_optimal_policy=True,step_pause=0.2)
-    # print("Q-value iteration, iteration {}, max error {}".format(i,max_error))
+
+    # TO DO: IMPLEMENT Q-VALUE ITERATION HERE
+    i = 0
+    while True:
+        max_error = 0
+        for s in range(env.n_states):
+            for a in range(env.n_actions):
+                q_old = QIagent.Q_sa[s, a]
+                p_sas, r_sas = env.model(s, a)
+                QIagent.update(s, a, p_sas, r_sas)
+                max_error = max(max_error, abs(QIagent.Q_sa[s, a] - q_old))
+        i += 1
+
+        # Plot current Q-value estimates & print max error
+        env.render(Q_sa=QIagent.Q_sa, plot_optimal_policy=True, step_pause=0.2)
+        print("Q-value iteration, iteration {}, max error {}".format(i, max_error))
+
+        if max_error < threshold:
+            break
  
     return QIagent
 
@@ -61,7 +72,18 @@ def experiment():
         s = s_next
 
     # TO DO: Compute mean reward per timestep under the optimal policy
-    # print("Mean reward per timestep under optimal policy: {}".format(mean_reward_per_timestep))
+    total_reward = 0
+    total_steps = 0
+    done = False
+    s = env.reset()
+    while not done:
+        a = QIagent.select_action(s)
+        s_next, r, done = env.step(a)
+        total_reward += r
+        total_steps += 1
+        s = s_next
+    mean_reward_per_timestep = total_reward / total_steps
+    print(f"Mean reward per timestep under optimal policy: {mean_reward_per_timestep}")
     
 if __name__ == '__main__':
     experiment()
